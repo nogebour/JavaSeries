998b48bdcc04dcf26121464706a363b11827365d
nicolas noel <nicolasnoelpro@laposte.net>
2014-12-22 11:42:52 +0100

    Create Main.java

---
+++ test/Main.java
@@ -0,0 +1,21 @@
+import java.io.InputStream;
+import junit.framework.TestCase;
+import jsontobom.ConvJSONToBOM;
+import bom.BSMember;
+import bom.BSShowCenter;
+import brequest.BetaSeriesReq;
+import brequest.HttpURLConnectionImpl;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Init Main");
+		BSMember member = new BSMember("Dev010", "developer");
+		ConvJSONToBOM conv = new ConvJSONToBOM();
+		BetaSeriesReq req = new BetaSeriesReq();
+		System.out.println("\nTesting 1 - Send Connection request to BetaSeries");
+		conv.getUserConnectionInfos(member);
+		System.out.println("\nTesting 1 - Send Remaining Episodes request to BetaSeries");
+		conv.getUserRemainingEpisodes(member);
+		System.out.println(BSShowCenter.INSTANCE.dump());
+	}
+}
