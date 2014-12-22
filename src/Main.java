9d1ad130bd4d6d7e0eb82ea6e70432faa2bb59dd
nicolas noel <nicolasnoelpro@laposte.net>
2014-12-22 10:27:18 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,13 @@ import brequest.HttpURLConnectionImpl;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-
-		HttpURLConnectionImpl http = new HttpURLConnectionImpl();
-
-		System.out.println("Testing 1 - Send Http GET request");
-		http.sendGet("http://www.google.com/search?q=mkyong", null, false);
-
-		System.out.println("\nTesting 2 - Send Http POST request");
-		http.sendPost("https://selfsolve.apple.com/wcResults.do", "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345", true);
-
-		System.out.println("\nTesting 3 - Send Http POST request to BetaSeries");
+		System.out.println("Init Main");
 		BSMember member = new BSMember("nogebour", "");
 		ConvJSONToBOM conv = new ConvJSONToBOM();
 		BetaSeriesReq req = new BetaSeriesReq();
+		System.out.println("\nTesting 1 - Send Connection request to BetaSeries");
 		conv.getUserConnectionInfos(member);
+		System.out.println("\nTesting 1 - Send Remaining Episodes request to BetaSeries");
 		conv.getUserRemainingEpisodes(member);
 		System.out.println(BSShowCenter.INSTANCE.dump());
 	}
