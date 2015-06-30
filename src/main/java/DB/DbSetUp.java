957982e9c46394cf046c4edd9fff7139fa6ca7f9
nogebour <nicolasnoelpro@laposte.net>
2015-06-30 17:39:33 +0200

    Add all create sql script

--- src/main/java/DB/DbSetUp.java
+++ src/main/java/DB/DbSetUp.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import junit.framework.Assert;
 import utils.preferences.UserPreferences;
 
 public class DbSetUp {	
@@ -66,10 +67,73 @@ public class DbSetUp {
 					+ "LANGUAGE text,"
 					+ "NOTETOTAL int,"
 					+ "NOTEMEAN int,"
-					+ "URL STRING,"
+					+ "URL text,"
+					+ "LAST_USAGE datetime,"
 					+ "primary key (ID));";
-			ResultSet theResultSet = executeSqlQuery(iConn, sqlCreateShow);
-			String sqlCreateMember = "Create table Member ";
+			String sqlCreateGenre = "Create table if not exists GENRE ("
+					+ "ID int,"
+					+ "GENRE text,"
+					+ "primary key (ID));";
+			String sqlCreateShowGenreMapping = "Create table if not exists SHOW_GENRE_MAPPING ("
+					+ "ID_SHOW int,"
+					+ "ID_GENRE int,"
+					+ "primary key (ID_SHOW, ID_GENRE),"
+					+ "foreign key (ID_SHOW) references SHOW(ID),"
+					+ "foreign key (ID_GENRE) references GENRE(ID));";
+			String sqlCreateEpisode = "Create table if not exists EPISODE ("
+					+ "ID int,"
+					+ "THETVDBID int,"
+					+ "TITLE text,"
+					+ "SEASON int,"
+					+ "EPISODE int,"
+					+ "ID_SHOW int,"
+					+ "CODE text,"
+					+ "GLOBAL_NUMBER int,"
+					+ "DESCRIPTION text,"
+					+ "AIRING_DATE datetime,"
+					+ "LAST_USAGE datetime,"
+					+ "foreign key (ID_SHOW) references SHOW(ID),"
+					+ "primary key (ID));";
+			String sqlCreateMember = "Create table if not exists MEMBER ("
+					+ "ID int,"
+					+ "PSEUDO text,"
+					+ "TOKEN text,"
+					+ "primary key (ID));";
+			String sqlCreateMemberShow = "Create table if not exists MEMBER_SHOW ("
+					+ "ID int auto_increment,"
+					+ "ID_MEMBER int,"
+					+ "ID_SHOW int,"
+					+ "NOTE int,"
+					+ "LAST_USAGE datetime,"
+					+ "primary key (ID),"
+					+ "foreign key (ID_MEMBER) references MEMBER(ID),"
+					+ "foreign key (ID_SHOW) references SHOW(ID));";
+			String sqlCreateRemainingEpisodes = "Create table if not exists REMAINING_EPISODES ("
+					+ "ID int,"
+					+ "ID_MEMBER_SHOW int,"
+					+ "ID_EPISODE int,"
+					+ "foreign key (ID_MEMBER_SHOW) references MEMBER_SHOW (ID),"
+					+ "foreign key (ID_EPISODE) references EPISODE (ID),"
+					+ "primary key (ID));";
+			String[] sqlCreateArray = {sqlCreateShow,
+					sqlCreateGenre,
+					sqlCreateShowGenreMapping,
+					sqlCreateEpisode,
+					sqlCreateMember,
+					sqlCreateMemberShow,
+					sqlCreateRemainingEpisodes};
+			String[] stringCreateArray = {"sqlCreateShow",
+					"sqlCreateGenre",
+					"sqlCreateShowGenreMapping",
+					"sqlCreateEpisode",
+					"sqlCreateMember",
+					"sqlCreateMemberShow",
+					"sqlCreateRemainingEpisodes"};
+			for(int anIndex = 0; anIndex < sqlCreateArray.length; ++anIndex){
+				int theResult = executeSqlQueryUpdate(iConn, sqlCreateArray[anIndex]);
+				System.out.println("Execution of "+stringCreateArray[anIndex]+" : "+theResult);
+				Assert.assertTrue(theResult == 0);
+			}
 		} catch (SQLException e) {
 			oStatus = false;
 			System.out.println("Exception during SQL Querys");
@@ -85,6 +149,12 @@ public class DbSetUp {
 			}
 		}
 		return oStatus;
+	}
+
+	private int executeSqlQueryUpdate(Connection iConn, String sqlQuery)
+			throws SQLException {
+		Statement theStmt = iConn.createStatement();
+		return theStmt.executeUpdate(sqlQuery);
 	}
 
 	private ResultSet executeSqlQuery(Connection iConn, String sqlQuery)
--- src/test/java/mainTest/DBTest.java
+++ src/test/java/mainTest/DBTest.java
@@ -1,5 +1,7 @@
 package mainTest;
 
+import java.sql.Connection;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,6 +9,8 @@ import DB.DbSetUp;
 import utils.preferences.UserPreferences;
 
 public class DBTest {
+	private Connection theConn;
+
 	@Test
 	public void connectTest(){
 		System.out.println("->Testing 1 - Default Value of DB name");
@@ -15,6 +19,21 @@ public class DBTest {
 			thePrefs.setSQLiteDBFile();
 			DbSetUp theDBClass = new DbSetUp();
 			Assert.assertNotNull(theDBClass.connectToDB());
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void initiateTest(){
+		System.out.println("->Testing 2 - Initiate DB");
+		try {
+			UserPreferences thePrefs = new UserPreferences();
+			thePrefs.setSQLiteDBFile();
+			DbSetUp theDBClass = new DbSetUp();
+			theConn = theDBClass.connectToDB();
+			Assert.assertTrue(theDBClass.initiateDataBase(theConn));
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail();
